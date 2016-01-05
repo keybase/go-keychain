9cf53c87839ca21086dcaa22d45804df060d6052
Gabriel Handford <gabrielh@gmail.com>
2016-01-05 14:19:29 -0800

    QueryItemRef and DeleteItemRef for dealing with references directly. Also double check result type to be safe.

--- test/osx_test.go
+++ test/osx_test.go
@@ -22,12 +22,38 @@ func TestAccess(t *testing.T) {
 	}
 }
 
-func TestGetGenericPassword(t *testing.T) {
-	password, err := keychain.GetGenericPassword("TestGetGenericPassword", "test", "", "")
+func TestGenericPasswordRef(t *testing.T) {
+	service, account, label, accessGroup, password := "TestGenericPasswordRef", "test", "", "", "toomanysecrets"
+
+	item := keychain.NewGenericPassword(service, account, label, []byte(password), accessGroup)
+	defer keychain.DeleteItem(item)
+	err := keychain.AddItem(item)
 	if err != nil {
 		t.Fatal(err)
 	}
-	if password != nil {
-		t.Fatal("Should be nil")
+
+	// Query reference and delete by reference
+	query := keychain.NewItem()
+	query.SetSecClass(keychain.SecClassGenericPassword)
+	query.SetService(service)
+	query.SetAccount(account)
+	query.SetMatchLimit(keychain.MatchLimitOne)
+	query.SetReturnRef(true)
+	ref, err := keychain.QueryItemRef(query)
+	if err != nil {
+		t.Fatal(err)
+	} else if ref == nil {
+		t.Fatal("Missing result")
+	} else {
+		err = keychain.DeleteItemRef(ref)
+		if err != nil {
+			t.Fatal(err)
+		}
+		keychain.Release(ref)
+	}
+
+	passwordAfter, err := keychain.GetGenericPassword(service, account, label, accessGroup)
+	if passwordAfter != nil {
+		t.Fatal("Shouldn't have password")
 	}
 }
