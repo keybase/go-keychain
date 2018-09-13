6d31e3c731f13afb4a925a38950dc82ed53b0bb9
Frederick Akalin <akalin@keyba.se>
2018-09-13 00:03:53 -0700

    Another

--- datetime.go
+++ datetime.go
@@ -16,12 +16,28 @@ import (
 const nsPerSec = 1000000000
 
 func unixToAbsoluteTime(s int64, ns int64) C.CFAbsoluteTime {
-	return C.CFTimeInterval(ns/nsPerSec) + C.CFAbsoluteTime(s) + C.kCFAbsoluteTimeIntervalSince1970
+	return C.CFTimeInterval(ns)/nsPerSec + C.CFAbsoluteTime(s) + C.kCFAbsoluteTimeIntervalSince1970
 }
 
 func absoluteTimeToUnix(abs C.CFAbsoluteTime) (int64, int64) {
 	int, frac := math.Modf(float64(abs - C.kCFAbsoluteTimeIntervalSince1970))
 	return int64(int), int64(frac * nsPerSec)
+}
+
+func absoluteTimeToString(abs C.CFAbsoluteTime) string {
+	dateFormatter := C.CFDateFormatterCreate(C.kCFAllocatorDefault, C.CFLocaleCopyCurrent(), C.kCFDateFormatterFullStyle, C.kCFDateFormatterFullStyle)
+	defer Release(C.CFTypeRef(dateFormatter))
+	cfStr := C.CFDateFormatterCreateStringWithAbsoluteTime(C.kCFAllocatorDefault, dateFormatter, abs)
+	defer Release(C.CFTypeRef(cfStr))
+	return CFStringToString(cfStr)
+}
+
+func cfDateToString(d C.CFDateRef) string {
+	dateFormatter := C.CFDateFormatterCreate(C.kCFAllocatorDefault, C.CFLocaleCopyCurrent(), C.kCFDateFormatterFullStyle, C.kCFDateFormatterFullStyle)
+	defer Release(C.CFTypeRef(dateFormatter))
+	cfStr := C.CFDateFormatterCreateStringWithDate(C.kCFAllocatorDefault, dateFormatter, d)
+	defer Release(C.CFTypeRef(cfStr))
+	return CFStringToString(cfStr)
 }
 
 func releaseCFDateForTest(d C.CFDateRef) {
