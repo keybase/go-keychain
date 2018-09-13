07a40e6ad2b75c57a1580bfa1e9d0ef9993f2a67
Frederick Akalin <akalin@keyba.se>
2018-09-13 00:55:08 -0700

    Add 1.9.x. hack

--- datetime.go
+++ datetime.go
@@ -21,7 +21,8 @@ func absoluteTimeIntervalSince1970() int64 {
 
 func unixToAbsoluteTime(s int64, ns int64) C.CFAbsoluteTime {
 	abs := s - absoluteTimeIntervalSince1970()
-	return C.CFAbsoluteTime(abs) + C.CFTimeInterval(ns)/nsPerSec
+	// Need to cast CFTimeInterval to CFAbsoluteTime for go 1.9.x.
+	return C.CFAbsoluteTime(abs) + C.CFAbsoluteTime(C.CFTimeInterval(ns))/nsPerSec
 }
 
 func absoluteTimeToUnix(abs C.CFAbsoluteTime) (int64, int64) {
