562bee3906cedf566634142327fb393b6ab0abdf
Frederick Akalin <akalin@keyba.se>
2018-09-13 00:27:29 -0700

    Better test

--- datetime_test.go
+++ datetime_test.go
@@ -3,16 +3,35 @@
 package keychain
 
 import (
+	"math"
 	"testing"
 	"time"
 )
 
+// Test time is 2018-09-13T06:08:49+00:00
+const (
+	// Number of seconds between the test time and the Unix epoch
+	// (1970-01-01T00:00:00+00:00).
+	testTimeUnixSeconds = 1536818929
+	// Number of seconds between the test time and
+	// CoreFoundation's absolute reference date
+	// (1 Jan 2001  00:00:00 GMT). See
+	// https://developer.apple.com/documentation/corefoundation/cfabsolutetime?language=objc
+	testTimeAbsoluteTimeSeconds = 558511729
+)
+
 func TestUnixToAbsoluteTime(t *testing.T) {
-	// 2018-09-13T06:08:49.123456789+00:00
-	var s int64 = 1536818929
-	var ns int64 = 123456789
-	abs := unixToAbsoluteTime(s, ns)
-	t.Logf("date is %s", absoluteTimeToString(abs))
+	var testNano int64 = 123456789
+	abs := unixToAbsoluteTime(testTimeUnixSeconds, testNano)
+	int, frac := math.Modf(float64(abs))
+	s := int64(int)
+	ns := int64(frac * nsPerSec)
+	if s != testTimeAbsoluteTimeSeconds {
+		t.Fatalf("expected %d, got %d", testTimeAbsoluteTimeSeconds, s)
+	}
+	if ns != testNano {
+		t.Fatalf("expected %d, got %d", testNano, ns)
+	}
 }
 
 func TestTimeToCFDateToTime(t *testing.T) {
