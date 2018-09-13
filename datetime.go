85a57c81a3e9341336dd57c27d6b160ef1db0daf
Frederick Akalin <akalin@keyba.se>
2018-09-13 00:23:55 -0700

    Fixup

--- datetime.go
+++ datetime.go
@@ -16,15 +16,15 @@ import (
 const nsPerSec = 1000000000
 
 func unixToAbsoluteTime(s int64, ns int64) C.CFAbsoluteTime {
-	return C.CFTimeInterval(ns)/nsPerSec + C.CFAbsoluteTime(s) + C.kCFAbsoluteTimeIntervalSince1970
+	return C.CFTimeInterval(ns)/nsPerSec + C.CFAbsoluteTime(s) - C.kCFAbsoluteTimeIntervalSince1970
 }
 
 func absoluteTimeToUnix(abs C.CFAbsoluteTime) (int64, int64) {
-	int, frac := math.Modf(float64(abs - C.kCFAbsoluteTimeIntervalSince1970))
+	int, frac := math.Modf(float64(abs + C.kCFAbsoluteTimeIntervalSince1970))
 	return int64(int), int64(frac * nsPerSec)
 }
 
-func absoluteTimeToString(abs C.CFAbsoluteTime) string {
+func absoluteTimeToDebugString(abs C.CFAbsoluteTime) string {
 	dateFormatter := C.CFDateFormatterCreate(C.kCFAllocatorDefault, C.CFLocaleCopyCurrent(), C.kCFDateFormatterFullStyle, C.kCFDateFormatterFullStyle)
 	defer Release(C.CFTypeRef(dateFormatter))
 	cfStr := C.CFDateFormatterCreateStringWithAbsoluteTime(C.kCFAllocatorDefault, dateFormatter, abs)
