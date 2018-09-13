bcee653db0273d30367da2fb1d2d27fd41c53179
Frederick Akalin <akalin@keyba.se>
2018-09-12 23:48:26 -0700

    More

--- datetime.go
+++ datetime.go
@@ -13,21 +13,28 @@ import (
 	"time"
 )
 
+const nsPerSec = 1000000000
+
+func unixToAbsoluteTime(s int64, ns int64) C.CFAbsoluteTime {
+	return C.CFTimeInterval(ns/nsPerSec) + C.CFAbsoluteTime(s) + C.kCFAbsoluteTimeIntervalSince1970
+}
+
+func absoluteTimeToUnix(abs C.CFAbsoluteTime) (int64, int64) {
+	int, frac := math.Modf(float64(abs - C.kCFAbsoluteTimeIntervalSince1970))
+	return int64(int), int64(frac * nsPerSec)
+}
+
 func releaseCFDateForTest(d C.CFDateRef) {
 	Release(C.CFTypeRef(d))
 }
 
-func TimeToCFDate(t time.Time) (C.CFDateRef, error) {
-	unixNano := t.UnixNano()
-	abs := C.CFAbsoluteTime(unixNano)/1000000000 + C.kCFAbsoluteTimeIntervalSince1970
-	return C.CFDateCreate(C.kCFAllocatorDefault, abs), nil
+func TimeToCFDate(t time.Time) C.CFDateRef {
+	abs := unixToAbsoluteTime(t.Unix(), int64(t.Nanosecond()))
+	return C.CFDateCreate(C.kCFAllocatorDefault, abs)
 }
 
-func CFDateToTime(d C.CFDateRef) (time.Time, error) {
+func CFDateToTime(d C.CFDateRef) time.Time {
 	abs := C.CFDateGetAbsoluteTime(d)
-	unix := float64(abs - C.kCFAbsoluteTimeIntervalSince1970)
-	int, frac := math.Modf(unix)
-	s := int64(int)
-	ns := int64(frac * 1000000000)
-	return time.Unix(s, ns), nil
+	s, ns := absoluteTimeToUnix(abs)
+	return time.Unix(s, ns)
 }
--- datetime_test.go
+++ datetime_test.go
@@ -10,16 +10,10 @@ import (
 func TestTimeToCFDateToTime(t *testing.T) {
 	// 2018-09-13T06:08:49.123456789+00:00
 	tm := time.Unix(1536818929, 123456789)
-	date, err := TimeToCFDate(tm)
-	if err != nil {
-		t.Fatal(err)
-	}
+	date := TimeToCFDate(tm)
 	defer releaseCFDateForTest(date)
 
-	tm2, err := CFDateToTime(date)
-	if err != nil {
-		t.Fatal(err)
-	}
+	tm2 := CFDateToTime(date)
 
 	if tm != tm2 {
 		t.Fatalf("expected %s, got %s", tm, tm2)
