fb6de365d9e523009d9819b02098aea866299137
Frederick Akalin <akalin@keyba.se>
2018-09-13 00:03:53 -0700

    Another

--- datetime_test.go
+++ datetime_test.go
@@ -7,10 +7,19 @@ import (
 	"time"
 )
 
+func TestUnixToAbsoluteTime(t *testing.T) {
+	// 2018-09-13T06:08:49.123456789+00:00
+	var s int64 = 1536818929
+	var ns int64 = 123456789
+	abs := unixToAbsoluteTime(s, ns)
+	t.Logf("date is %s", absoluteTimeToString(abs))
+}
+
 func TestTimeToCFDateToTime(t *testing.T) {
 	// 2018-09-13T06:08:49.123456789+00:00
 	tm := time.Unix(1536818929, 123456789)
 	date := TimeToCFDate(tm)
+	t.Logf("date is %s", cfDateToString(date))
 	defer releaseCFDateForTest(date)
 
 	tm2 := CFDateToTime(date)
