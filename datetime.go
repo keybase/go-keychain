db46bc812cc02343490a45c9b548fdf9b5dc5350
Frederick Akalin <akalin@keyba.se>
2018-09-12 23:36:57 -0700

    Got sorta working

--- datetime.go
+++ datetime.go
@@ -8,16 +8,26 @@ package keychain
 #include <CoreFoundation/CoreFoundation.h>
 */
 import "C"
-import "time"
+import (
+	"math"
+	"time"
+)
 
 func releaseCFDateForTest(d C.CFDateRef) {
 	Release(C.CFTypeRef(d))
 }
 
 func TimeToCFDate(t time.Time) (C.CFDateRef, error) {
-	panic("Unimplemented")
+	unixNano := t.UnixNano()
+	abs := C.CFAbsoluteTime(unixNano)/1000000000 + C.kCFAbsoluteTimeIntervalSince1970
+	return C.CFDateCreate(C.kCFAllocatorDefault, abs), nil
 }
 
 func CFDateToTime(d C.CFDateRef) (time.Time, error) {
-	panic("Unimplemented")
+	abs := C.CFDateGetAbsoluteTime(d)
+	unix := float64(abs - C.kCFAbsoluteTimeIntervalSince1970)
+	int, frac := math.Modf(unix)
+	s := int64(int)
+	ns := int64(frac * 1000000000)
+	return time.Unix(s, ns), nil
 }
